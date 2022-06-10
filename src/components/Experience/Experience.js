1eb645d6e626c27d9c27a20abb1945d5d8f0a192
HossainRafi <hossainrafi71@gmail.com>
2022-06-11 02:01:54 +0600

    skills done

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,11 +1,103 @@
-import React from 'react';
+import React from "react";
+import "./Experience.css"
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-    return (
-        <section id='experience'>
-            <h5>Experience</h5>
-        </section>
-    );
+  return (
+    <section id="experience">
+      <h5>Get To Know</h5>
+      <h2>My Skills</h2>
+      <div className="container experience-container">
+        <div className="frontend">
+          <h3>Frontend Technologies</h3>
+          <div className="experience-content">
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>React.js</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="backend">
+          <h3>Backend Technologies</h3>
+          <div className="experience-content">
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>Node.js</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>Express.js</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>REST API</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsPatchCheckFill className="experience-details-icon" />
+              <div>
+                <h4>NoSQL</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
